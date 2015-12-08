94cb2a1b85c9f39093736030d1322de32ae4620e
alexlindsay <alexlrocks@gmail.com>
2015-12-07 23:27:21 -0800

    Added notification listener code in LIstenerService

--- wear/src/main/java/group19/cs160/scoreradar/ListenerService.java
+++ wear/src/main/java/group19/cs160/scoreradar/ListenerService.java
@@ -1,46 +1,64 @@
 package group19.cs160.scoreradar;
 
-import android.content.Intent;
-import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
+        import android.app.Notification;
+        import android.app.PendingIntent;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.v4.app.NotificationCompat;
+        import android.support.v4.app.NotificationManagerCompat;
+        import android.util.Log;
 
-import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.MessageEvent;
-import com.google.android.gms.wearable.WearableListenerService;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+        import com.google.android.gms.wearable.WearableListenerService;
+        import com.google.gson.Gson;
+        import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
+        import java.lang.reflect.Type;
+        import java.util.ArrayList;
 
-/**
- * Created by alexlrocks on 12/7/15.
- */
+        import group19.cs160.scoreradar.R;
+        import pl.tajchert.buswear.EventBus;
+
 public class ListenerService extends WearableListenerService {
 
     @Override
-    public void onMessageReceived(MessageEvent messageEvent) {
+    public void onCreate() {
+        super.onCreate();
+        EventBus.getDefault().register(this);
+    }
 
-        if (messageEvent.getPath().equals("/message_path")) {
-            final String message = new String(messageEvent.getData());
-            Log.e("in listenerService", message);
+    public void onEvent(String response) {
+        if (response.startsWith("WearUpdate")) {
+            Log.d("in listener-onEvent", "Here in event for notification");
+            response = response.substring(10);
             Gson gson = new Gson();
-            // Convert it back to normal here
-            Type listOfTemp = new TypeToken<ArrayList<Game>>(){}.getType();
+            Type listOfTemp = new TypeToken<ArrayList<Game>>() {
+            }.getType();
+            ArrayList<Game> gameList = gson.fromJson(response, listOfTemp);
 
-            // Broadcast message to wearable activity for display
-            Intent messageIntent = new Intent();
-            messageIntent.setAction(Intent.ACTION_SEND);
-            messageIntent.putExtra("message in here", message);
-            LocalBroadcastManager.getInstance(this).sendBroadcast(messageIntent);
-        }
-        else {
-            super.onMessageReceived(messageEvent);
+
+            int notificationId = 001;
+            // Build intent for notification content
+            Intent viewIntent = new Intent(this, MainActivity.class);
+            //viewIntent.putExtra("gameList", response);
+            PendingIntent viewPendingIntent =
+                    PendingIntent.getActivity(this, 0, viewIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            NotificationCompat.Builder notificationBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setContentTitle("New Score Update From ScoreRadar")
+                            .setContentText("Click here to see your games!")
+                            .setDefaults(Notification.DEFAULT_ALL)
+                            .extend(new NotificationCompat.WearableExtender().setDisplayIntent(viewPendingIntent));
+
+            // Get an instance of the NotificationManager service
+            NotificationManagerCompat notificationManager =
+                    NotificationManagerCompat.from(this);
+
+            // Build the notification and issues it with notification manager.
+            notificationManager.notify(notificationId, notificationBuilder.build());
         }
     }
 
-    @Override
-    public void onDataChanged(DataEventBuffer dataEvents) {
-
-    }
 }
