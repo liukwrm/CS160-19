ec01e2535fe91b5e74ee54ebb9df35eccb9dca1d
alexlindsay <alexlrocks@gmail.com>
2015-12-08 17:43:58 -0800

    Notifications open wear app

---
+++ wear/src/main/java/group19/cs160/scoreradar/NotificationActivity.java
@@ -0,0 +1,14 @@
+package group19.cs160.scoreradar;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NotificationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+
+}
